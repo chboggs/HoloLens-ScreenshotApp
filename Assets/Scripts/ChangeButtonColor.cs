c92e17713dda00e78bfd550d7a13c72e2ae91220
Cody Nagel <nagelc@umich.edu>
2017-10-26 16:15:07 -0400

    Button prefab and should change color on gaze

---
+++ Assets/Scripts/ChangeButtonColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButtonColor : MonoBehaviour {
+
+    // Use this for initialization
+    private void Start()
+    {
+        GazeReceiver gr = GetComponent<GazeReceiver>();
+        gr.GazeEnterEvent.AddListener(ChangeCursorOn);
+        gr.GazeLeaveEvent.AddListener(ChangeCursorAway);
+    }
+
+    public void ChangeCursorOn (Ray r) {
+        GetComponent<Renderer>().material.SetColor("blue", Color.blue);
+	}
+	
+	// Update is called once per frame
+	public void ChangeCursorAway (Ray r) {
+        GetComponent<Renderer>().material.SetColor("white", Color.white);
+    }
+}
